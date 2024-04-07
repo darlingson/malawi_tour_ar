da55933d6e045b4eff03014e7509570c0a8b785f
darlingson <dmakuwila@gmail.com>
2024-04-07 18:01:32 +0200

    removed the static and replaced it with usenavigate

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,15 @@
 import '../App.css';
+import { useNavigate } from "react-router-dom";
 const Home = () =>{
+    const navigate = useNavigate();
     return(
         <>
         <div className="text-3xl font-bold underline text-red-600">
-            Hello
-            <h1>Hi click here <a href="/ar">Ar</a></h1>
+            <button
+                onClick={() => navigate("/ar")}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >AR
+            </button>
         </div>
         </>
     );
